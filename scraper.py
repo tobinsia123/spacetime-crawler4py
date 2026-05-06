d8d4c022941d9baca1bb38e1623c56abdd4dd415
Tobin Sia <txsia@uci.edu>
2026-05-06 00:13:53 -0700

    [fix] added more stop words

--- scraper.py
+++ scraper.py
@@ -14,8 +14,26 @@ seen_exact = set()
 seen_near = []
 
 STOP_WORDS = {
-    "the","a","and","of","to","in","is","for","on","with",
-    "that","by","this","it","from","as","at","be","are","was","were"
+    "a","about","above","after","again","against","all","am","an","and",
+    "any","are","aren't","as","at","be","because","been","before","being",
+    "below","between","both","but","by","can't","cannot","could","couldn't",
+    "did","didn't","do","does","doesn't","doing","don't","down","during",
+    "each","few","for","from","further","had","hadn't","has","hasn't",
+    "have","haven't","having","he","he'd","he'll","he's","her","here",
+    "here's","hers","herself","him","himself","his","how","how's","i",
+    "i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's",
+    "its","itself","let's","me","more","most","mustn't","my","myself",
+    "no","nor","not","of","off","on","once","only","or","other","ought",
+    "our","ours","ourselves","out","over","own","same","shan't","she",
+    "she'd","she'll","she's","should","shouldn't","so","some","such",
+    "than","that","that's","the","their","theirs","them","themselves",
+    "then","there","there's","these","they","they'd","they'll","they're",
+    "they've","this","those","through","to","too","under","until","up",
+    "very","was","wasn't","we","we'd","we'll","we're","we've","were",
+    "weren't","what","what's","when","when's","where","where's","which",
+    "while","who","who's","whom","why","why's","with","won't","would",
+    "wouldn't","you","you'd","you'll","you're","you've","your","yours",
+    "yourself","yourselves"
 }
 
 def scraper(url, resp):
@@ -156,7 +174,7 @@ def is_valid(url):
         if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             r"|png|tiff?|mid|mp2|mp3|mp4"
-            r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ipynb"
             r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             r"|epub|dll|cnf|tgz|sha1"
